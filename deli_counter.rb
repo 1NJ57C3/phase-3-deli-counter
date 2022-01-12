3d0e2f403a349ad192b05908ce39ea0833d88d7a
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2022-01-11 20:50:16 -0800

    commit tree 2e756540dd53701659598b5cfe5715fa7be0b90d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+def line deli
+    if deli == []
+        puts "The line is currently empty."
+    else
+        puts "The line is currently:#{deli.map.with_index(1) {|person, i| " #{i}. #{person}"}.join}"
+    end
+end
+
+def take_a_number deli, name
+    deli << name
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving deli
+    if deli == []
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{deli[0]}."
+        deli.shift
+    end
+end
